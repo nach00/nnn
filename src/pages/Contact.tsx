7b7becf629c13ceb10704ee2a7ab8200bd3e8008
nach00 <bignacho@gmail.com>
2024-02-08 05:07:11 -0600

    🔥 Remove more animations

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -196,14 +196,6 @@ const EmailForm = () => {
             bordered
             elevate
             key="content"
-            animation={[
-              'quick',
-              {
-                opacity: {
-                  overshootClamping: true,
-                },
-              },
-            ]}
             enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
             exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
             x={0}
@@ -227,56 +219,5 @@ const EmailForm = () => {
         </AlertDialog.Portal>
       </AlertDialog>
     </Form>
-  )
-}
-export function Success() {
-  return (
-    <AlertDialog>
-      {/*<AlertDialog.Trigger asChild>*/}
-      {/*  <Button>Show Alert</Button>*/}
-      {/*</AlertDialog.Trigger>*/}
-
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay
-          key="overlay"
-          animation="lazy"
-          opacity={0.5}
-          enterStyle={{ opacity: 0 }}
-          exitStyle={{ opacity: 0 }}
-        />
-        <AlertDialog.Content
-          bordered
-          elevate
-          key="content"
-          animation={[
-            'quick',
-            {
-              opacity: {
-                overshootClamping: true,
-              },
-            },
-          ]}
-          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
-          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
-          x={0}
-          scale={1}
-          opacity={1}
-          y={0}
-        >
-          <YStack gap="$3">
-            <AlertDialog.Title>Success!</AlertDialog.Title>
-            <AlertDialog.Description>
-              Thanks for reaching out! I'll get back to you as soon as possible.
-            </AlertDialog.Description>
-
-            <XStack gap="$3" justifyContent="flex-end">
-              <AlertDialog.Action asChild>
-                <Button theme="active">Dismiss</Button>
-              </AlertDialog.Action>
-            </XStack>
-          </YStack>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog>
   )
 }
